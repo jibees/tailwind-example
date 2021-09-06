d664113434b8aee9dcd80987718d7cbb9a457ff9
Jean-Baptiste Bellet <jb.bellet@gmail.com>
2021-09-06 15:31:07 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: "jit",
+  purge: {
+    enabled: true,
+    content: [
+      "./app/views/**/*.html.erb",
+      "./app/views/**/*.html.haml",
+      "./app/components/**/*.html.haml",
+      "./app/assets/javascript/templates/**/*.html.haml",
+      "./engines/**/*.html.haml",
+    ],
+    options: {
+      defaultExtractor: (content) =>
+        content.match(/[^%#<>"{\.'`\s]*[^%#<>"{}\.'`\s:]/g) || [],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
